6699b3e799121442ba440f0cee717882b7e13e63
kyrstenjunior <kyrsten.junior@nairuz.com.br>
2024-03-23 09:30:07 -0300

    finalização das rotas user e criação category

---
+++ backend/src/controllers/category/CreateCategoryController.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from "express";
+
+class CreateCategoryController {
+    async handle(req: Request, res: Response) {
+
+    }
+}
+
+export { CreateCategoryController }
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,13 +1,21 @@
 import { Router, Request, Response } from "express";
+
+// controllers
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
+import { DetailUserController } from "./controllers/user/DetailUserController";
+
+// Middlewares
+import { isAuthenticated } from "./middlewares/isAuthenticated";
 
 const router = Router();
 
 /* ---------- Rotas User ---------- */ 
-
 // Rota chama o Controller, Controller chama o Service que executa a requisição e vem devolvendo os retornos
 router.post('/users', new CreateUserController().handle);
 router.post('/session', new AuthUserController().handle);
+router.get('/me', isAuthenticated, new DetailUserController().handle);
+
+/* ---------- Rotas Category ---------- */ 
 
 export { router };
